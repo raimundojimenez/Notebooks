6fc60c6b81e64dece101d7a1953554ef4c87240f
Warwick McNaughton <warwick.mcnaughton@gmail.com>
2020-01-12 10:37:04 +1300

    Text-align

--- Cryptography/MyModules/mystyles.py
+++ Cryptography/MyModules/mystyles.py
@@ -4,5 +4,5 @@ def table_style():
    # To style tables over-riding Jupyter css script styles (Jupyter lab, nbviewer)
     display(HTML("<style>\
         .jp-RenderedHTMLCommon table { font-size: 14px;display:inline-block;text-align:left;}.jp-RenderedHTMLCommon th { min-width:90px;max-width:80%;text-align:left;}\
-        .rendered_html table { font-size: 14px;display:inline-block;}.rendered_html th { min-width:90px;max-width:80%;}\
+        .rendered_html table { font-size: 14px;display:inline-block;text-align:left;}.rendered_html th { min-width:90px;max-width:80%;text-align:left;}\
     </style>"))
