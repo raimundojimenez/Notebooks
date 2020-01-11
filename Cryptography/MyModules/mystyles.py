f41110d460026d89743d208b0e3667f9255cceb0
Warwick McNaughton <warwick.mcnaughton@gmail.com>
2020-01-12 10:19:40 +1300

    Text-align left

--- Cryptography/MyModules/mystyles.py
+++ Cryptography/MyModules/mystyles.py
@@ -1,8 +1,8 @@
 from IPython.core.display import display, HTML
 
 def table_style():
-   # To style tables
+   # To style tables over-riding Jupyter css script styles (Jupyter lab, nbviewer)
     display(HTML("<style>\
-        .jp-RenderedHTMLCommon table { font-size: 14px;display:inline-block;}.jp-RenderedHTMLCommon th { min-width:90px;max-width:80%;}\
+        .jp-RenderedHTMLCommon table { font-size: 14px;display:inline-block;text-align:left;}.jp-RenderedHTMLCommon th { min-width:90px;max-width:80%;text-align:left;}\
         .rendered_html table { font-size: 14px;display:inline-block;}.rendered_html th { min-width:90px;max-width:80%;}\
     </style>"))
